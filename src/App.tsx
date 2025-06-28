037a0c3e05a1e5e8271f963da51c40f40d3bff56
Abpa007 <abhaypanchal095@gmail.com>
2025-06-28 10:55:59 +0530

    Initial commit: Redux Toolkit React project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import {Provider} from "react-redux"
+import store from "./Store/Store"
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      <Provider store={store}>
+        <p>hvsagdhgavshgd</p>
+      </Provider>
+    </>
+  )
+}
+
+export default App

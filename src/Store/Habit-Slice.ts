16e6d8d0f1299ab51ec9acd97753c83d9fabd76b
Abpa007 <abhaypanchal095@gmail.com>
2025-06-28 15:12:49 +0530

    Install Redux Toolkit, React-Redux, and Material-UI

--- src/Store/Habit-Slice.ts
+++ src/Store/Habit-Slice.ts
@@ -1,3 +1,28 @@
 import {createSlice} from "@reduxjs/toolkit";
 
-const habitSlice = createSlice
+export interface Habit {
+    id:string;
+    name:string;
+    frequency: "daily" | "weekly";
+    completeDates: string[];
+    createdAt: string;
+}
+interface HabitState {
+    habits :Habit[]
+}
+const initialState : HabitState={
+    habits:[]
+}
+
+const habitSlice = createSlice({
+    name:"habits",
+    initialState,
+    reducers:{
+        addHabit: () => {
+
+        }
+    }
+});
+
+export const {addHabit} = habitSlice.actions;
+export default habitSlice.reducer;
